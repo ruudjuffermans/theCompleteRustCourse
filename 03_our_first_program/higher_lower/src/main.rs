b60304b2aec7cf20faa4eb669a0ff20a16caa40e
Ruud Juffermans <ruud@ruuds-mbp.kpn>
2022-02-19 22:10:39 +0100

    added loop and error handling :bee: :hammer:

--- 03_our_first_program/higher_lower/src/main.rs
+++ 03_our_first_program/higher_lower/src/main.rs
@@ -12,27 +12,37 @@ fn main() {
     // print the random number
     println!("the secret number = {}", secret_number);
 
-    println!("please input your guess");
+    loop {
+        println!("please input your guess");
 
-    // initialize a variable
-    // let counter1 = 5; // immutable
-    // let mut counter2 = 5; // mutable
-    let mut guess = String::new();
+        // initialize a variable
+        // let counter1 = 5; // immutable
+        // let mut counter2 = 5; // mutable
+        let mut guess = String::new();
 
-    // store the user provided number
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
+        // store the user provided number
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
 
-    // convert string to number
-    let guess: u32 = guess.trim().parse().expect("please type a number!");
+        // convert string to number
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    // notify user of stored value
-    println!("we stored your guess: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // notify user of stored value
+        println!("we stored your guess: {}", guess);
+        // compare the two numbers
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+
+    println!("goodbye!");
 }
